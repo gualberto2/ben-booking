356f0d6fed77abdae5fa02629b397fdbfee5d12b
gualberto2 <elijahwilsonnn@gmail.com>
2024-07-20 10:47:15 -0700

    styling: dashboad layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
-export default function RootLayout({
+export default function DashLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      {/* Nav */}
-      <body>{children}</body>
-      {/* Footer */}
+    <main className="flex h-screen flex-col">
+      <header className="flex-shrink-0"></header>
+
+      <section className="flex flex-1 flex-row overflow-hidden">
+        <div className="w-full">{children}</div>
+      </section>
+
+      <footer className="flex-shrink-0"></footer>
     </main>
   );
 }
