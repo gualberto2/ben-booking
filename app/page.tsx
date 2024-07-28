c3851d3c5790aae97b0b65d53d81ec3231e236cf
gualberto2 <elijahwilsonnn@gmail.com>
2024-07-27 18:38:20 -0700

    added login button to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
 import { HeroHighlightDemo } from "@/components/hero-highlight-temp";
-import Image from "next/image";
+
+import { createClient } from "@/utils/supabase/server";
+
+import LoginButton from "@/components/auth-components/login";
 
 export default function Home() {
+  const canInitSupabaseClient = () => {
+    // This function is just for the interactive tutorial.
+    // Feel free to remove it once you have Supabase connected.
+    try {
+      createClient();
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const isSupabaseConnected = canInitSupabaseClient();
+
   return (
-    <div className="h-screen mx-auto  ">
+    <div className="h-screen mx-auto">
       <HeroHighlightDemo />
+
+      <div className="h-[40rem] flex justify-center">
+        <div className="flex flex-col items-center justify-center">
+          <LoginButton />
+        </div>
+      </div>
     </div>
   );
 }
