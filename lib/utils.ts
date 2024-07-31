3bb0a8f326bba077560594c4980758616c490900
gualberto2 <elijahwilsonnn@gmail.com>
2024-07-31 08:46:19 -0700

    shadcn: installed a bunch of components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
+import { Library } from "@googlemaps/js-api-loader";
 import { twMerge } from "tailwind-merge";
 
+export const libs: Library[] = ["core", "maps", "places", "marker"];
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
