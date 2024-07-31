aeef747145a3e82847c29a15dd75c39839364b69
gualberto2 <elijahwilsonnn@gmail.com>
2024-07-31 08:48:47 -0700

    google maps auto complete function with map, will use in form when user is creating a booking

---
+++ app/dashboard/book/components/map.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { useJsApiLoader } from "@react-google-maps/api";
+import { libs } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+
+type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+type AddressAutoCompleteInputProps = {
+  onAddressSelect: (address: string, gpscoords: LatLng) => void;
+  selectedAddress?: string;
+};
+
+function AddressAutoCompleteInput({
+  onAddressSelect,
+  selectedAddress,
+}: AddressAutoCompleteInputProps) {
+  const [autoComplete, setAutoComplete] =
+    useState<google.maps.places.Autocomplete | null>(null);
+
+  const { isLoaded } = useJsApiLoader({
+    nonce: "477d4456-f7b5-45e2-8945-5f17b3964752",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY!,
+    libraries: libs,
+  });
+
+  const placesAutoCompleteRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isLoaded) {
+      const gAutoComplete = new google.maps.places.Autocomplete(
+        placesAutoCompleteRef.current as HTMLInputElement,
+        {
+          fields: ["formatted_address", "geometry", "name"],
+          componentRestrictions: {
+            country: ["us"],
+          },
+        }
+      );
+
+      gAutoComplete.addListener("place_changed", () => {
+        const place = gAutoComplete.getPlace();
+        const position = place.geometry?.location;
+        onAddressSelect(place.formatted_address!, {
+          lat: position?.lat()!,
+          lng: position?.lng()!,
+        });
+      });
+    }
+  }, [isLoaded]);
+
+  useEffect(() => {
+    // https://github.com/radix-ui/primitives/issues/1859
+    // Disable Radix ui dialog pointer events lockout
+    setTimeout(() => (document.body.style.pointerEvents = ""), 0);
+  });
+
+  return <Input ref={placesAutoCompleteRef} defaultValue={selectedAddress} />;
+}
+
+export default AddressAutoCompleteInput;
