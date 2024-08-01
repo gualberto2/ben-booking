0c19ed8ca59d214b53d2c72f4b8cf2b700d616c7
gualberto2 <elijahwilsonnn@gmail.com>
2024-08-01 07:11:28 -0700

    types and utilities for google maps API

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,7 @@ import { Library } from "@googlemaps/js-api-loader";
 import { twMerge } from "tailwind-merge";
 
 export const libs: Library[] = ["core", "maps", "places", "marker"];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
