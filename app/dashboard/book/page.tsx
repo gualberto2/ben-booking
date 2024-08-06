6ba851927bb26656c707deb206914ac8118d29c5
gualberto2 <elijahwilsonnn@gmail.com>
2024-08-05 20:44:49 -0700

    Added heading and to new booking form

--- app/dashboard/book/page.tsx
+++ app/dashboard/book/page.tsx
@@ -1,11 +1,16 @@
-import BookingForm from "./components/booking-form";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { useRouter } from "next/navigation";
 
 const CreateBooking = () => {
+  const router = useRouter();
   return (
-    <section>
-      <div>
-        <BookingForm />
-        {/* <SpotAddress /> */}
+    <section className="">
+      <div className="flex flex-row justify-between items-center">
+        <Heading title="Booking" description="Manage your bookings here." />
+        <Button onClick={() => router.push(`/dashboard/book/new`)}>New</Button>
       </div>
     </section>
   );
